cc37689067fbe6e0f74775789892a2f9b3ad46e4
karalesus <karalesus@gmail.com>
2025-05-17 14:51:53 +0300

    add spring boot application class

---
+++ src/main/java/com/miit/spring_boot_test/NoodleChopsticksApplication.java
@@ -0,0 +1,13 @@
+package com.miit.spring_boot_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoodleChopsticksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoodleChopsticksApplication.class, args);
+    }
+
+}
